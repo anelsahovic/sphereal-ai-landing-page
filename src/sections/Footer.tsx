9b004d1bce9c6d2a9b3da1a4ec7d02cae5d65a58
Anel <anel.sahovic.bsc@gmail.com>
2024-12-31 14:45:34 +0100

    Create Footer Section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,49 +1,87 @@
-import React from "react";
+import React from 'react';
 import {
   faYoutube,
   faXTwitter,
   faDiscord,
-} from "@fortawesome/free-brands-svg-icons";
+} from '@fortawesome/free-brands-svg-icons';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const navItems = [
   {
-    name: "Features",
-    href: "#features",
+    name: 'Features',
+    href: '#features',
   },
   {
-    name: "Pricing",
-    href: "#pricing",
+    name: 'Pricing',
+    href: '#pricing',
   },
   {
-    name: "Testimonials",
-    href: "#testimonials",
+    name: 'Testimonials',
+    href: '#testimonials',
   },
   {
-    name: "Login",
-    href: "#login",
+    name: 'Login',
+    href: '#login',
   },
 ];
 
 export const socialLinks = [
   {
-    name: "Youtube",
+    name: 'Youtube',
     icon: faYoutube,
-    href: "#youtube",
+    href: '#',
   },
   {
-    name: "X",
+    name: 'X',
     icon: faXTwitter,
-    href: "#x",
+    href: '#',
   },
   {
-    name: "Discord",
+    name: 'Discord',
     icon: faDiscord,
-    href: "#discord",
+    href: '#',
   },
 ];
 
 export const Footer = () => {
-  return <footer>footer section</footer>;
+  return (
+    <footer className="border-t border-[var(--color-border)]">
+      <div className="container py-8">
+        <div className="flex flex-col lg:flex-row lg:justify-between items-center gap-8">
+          <div className="font-extrabold text-2xl">sphereal.ai</div>
+          <nav className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
+            {navItems.map((item) => (
+              <Link
+                href={item.href}
+                key={item.href}
+                className="uppercase text-xs tracking-widest font-bold text-gray-400"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </nav>
+        </div>
+
+        <div className="mt-16 flex flex-col md:flex-row-reverse md:justify-between items-center gap-8">
+          <div className="flex justify-center gap-6">
+            {socialLinks.map((link) => (
+              <Link key={link.name} href={link.href}>
+                <div className="size-10 rounded-full bg-gray-900 inline-flex items-center justify-center">
+                  <FontAwesomeIcon icon={link.icon} className="size-4 " />
+                </div>
+              </Link>
+            ))}
+          </div>
+
+          <div className="flex gap-2 items-center text-gray-500">
+            <small>@anelsahovic </small>
+            <small>All rights reserved</small>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
